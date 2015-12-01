8249f5b9fdac49c89d276fc850f2649a5d4f8d10
Luccan <luccan.ryanata@gmail.com>
2015-12-01 23:24:52 +0800

    Reverted some username changes, confirmed that 'applications' work after installing nodejs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @client = Mysql2::Client.new(:database => "datamie", :host => "localhost", :username => "root", :password => "")
+    @client = Mysql2::Client.new(:database => "datamie", :host => "localhost", :username => "datamie", :password => "")
     @users = @client.query("SELECT * FROM users")
   end
 
