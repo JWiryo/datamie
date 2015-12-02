d7398e4b1aee093843f8c4395f3afbe66533adb2
kfennedy <katherine.fennedy@gmail.com>
2015-12-03 03:20:05 +0800

    implement redirection after a successful post on rating, with a fancy flash message too ^.^

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,27 +1,28 @@
 class RatingsController < ApplicationController
 
   def create
-	if params[:id] != nil
-		@ratings = query_db("SELECT * FROM ratings WHERE Rating_ID = "+params[:id].to_s)
-		if @ratings.any?
-			query_db("UPDATE Ratings
-					SET Description = "+params[:Description].to_s+",
-					Score = "+params[:Score].to_s+",
-					Date = "+Time.now.to_s+"
-					WHERE B.Product_ID = "+params[:id].to_s+";")
-		else
-			#nothing
-		end
-	else
-		query_db("INSERT INTO Ratings (Product_ID, Username, Score, Date, Description)
-				SELECT "+params[:rating][:Product_ID].to_s+" AS Product_ID,
-				'"+params[:rating][:Username].to_s+"' AS Username,
-				"+params[:rating][:Score].to_s+" AS Score,
-				'"+Time.now.strftime("%Y-%m-%d %H:%M:%S")+"' AS Date,
-				'"+params[:rating][:Description].to_s+"' AS Description;")
-	end
+    if params[:id] != nil
+  		@ratings = query_db("SELECT * FROM ratings WHERE Rating_ID = "+params[:id].to_s)
+  		if @ratings.any?
+  			query_db("UPDATE Ratings
+  					SET Description = "+params[:Description].to_s+",
+  					Score = "+params[:Score].to_s+",
+  					Date = "+Time.now.to_s+"
+  					WHERE B.Product_ID = "+params[:id].to_s+";")
+        redirect_to url_for(:controller => 'products', :action=>'show', :id => params[:rating][:Product_ID])
+        flash[:success] = "Rating successfully edited."
+  		else
+  			#nothing
+  		end
+  	else
+  		query_db("INSERT INTO Ratings (Product_ID, Username, Score, Date, Description)
+  				SELECT "+params[:rating][:Product_ID].to_s+" AS Product_ID,
+  				'"+params[:rating][:Username].to_s+"' AS Username,
+  				"+params[:rating][:Score].to_s+" AS Score,
+  				'"+Time.now.strftime("%Y-%m-%d %H:%M:%S")+"' AS Date,
+  				'"+params[:rating][:Description].to_s+"' AS Description;")
+      redirect_to url_for(:controller => 'products', :action=>'show', :id => params[:rating][:Product_ID])
+      flash[:success] = "Rating successfully posted."
+  	end
   end
-  
-  #redirect_to url_for(:controller => 'products', :action => 'index') and return
-
 end
