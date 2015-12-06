a3a7011b330a82c3a75ee5f86889566230101a5f
kfennedy <katherine.fennedy@gmail.com>
2015-12-07 00:19:05 +0800

    refactoring of methods that will be used by all models to be implemented, just remember to inherit from AbstractModels.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,4 @@
-class Rating
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
+class Rating < AbstractModels
 
   attr_accessor :Score, :Description
 
@@ -11,24 +8,4 @@ class Rating
   validates_numericality_of :Score, greater_than_or_equal_to: 0, message: "cannot be negative"
   validates_numericality_of :Score, less_than_or_equal_to: 10, message: "cannot be greater than 10"
 
-  def initialize(params = {})
-    self.attributes = params unless params.nil? || params.empty?
-  end
-
-  def attributes=(params = {})
-    params.to_hash.symbolize_keys!
-    params.each do |k, v|
-      accessor_name = "#{k}="
-      if respond_to? accessor_name
-        send(accessor_name, v)
-      else
-        instance_variable_set("@#{k}", v)
-      end
-    end
-  end
-
-
-  def persisted?
-    false
-  end
 end
