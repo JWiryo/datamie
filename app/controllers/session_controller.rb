eaa17577197b2552e6135a5c83e2af4144536d7a
kfennedy <katherine.fennedy@gmail.com>
2015-12-06 21:44:30 +0800

    edit logic of authentication to ensure users with no matching username in the database does not lead to nullpointerexception

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,27 +3,36 @@ class SessionController < ApplicationController
 	def new
 	end
 
-	#nama function = nama view
+	def create
+		@session_username = params[:session][:username]
+		@session_password = params[:session][:password]
+		@database_username = query_db("SELECT Username FROM Users WHERE Username = '" +@session_username.to_s + "' ; " ).each(:as => :array).join
 
-	def authenticate
-		@client = Mysql2::Client.new(:database => "datamie", :host => "localhost", :username => "datamie", :password => "")
-		@session_username = params[:userdata][:Data_Username]
-		@session_password = params[:userdata][:Data_Password] 
-		#@LoginPassword = query_db("SELECT Password FROM users WHERE Username = " + @session_username.to_s + " ; " )
-		@LoginPassword = query_db("SELECT Password FROM users WHERE Username = '" + params[:userdata][:Data_Username].to_s + "' ; " ).each(:as => :array).join
-
-		if @LoginPassword == @session_password
-			redirect_to '/success'
+		if !@database_username.nil?
+			if authenticate(@database_username, @session_password)
+				redirect_to products_path
+				flash[:success] = "Hello #{@session_username}, you have successfully logged in."
+			else
+				flash.now[:danger] = "Invalid. Try again."
+				render 'new'
+			end
 		else
-			redirect_to '/authenticate'
+			flash.now[:danger] = "Invalid. Try again."
+			render 'new'
 		end
- 
+
 	end
 
-	def success
-	end
+	private
 
-	def fail
+	def authenticate(database_username, input_password)
+		@database_password = query_db("SELECT Password FROM users WHERE Username = '" + database_username.to_s + "' ; " ).each(:as => :array).join
+		if @database_password == input_password
+			return true
+		else
+			return false
+		end
+
 	end
 
 end
