836e95019ceb30df31adc862915a1988c111c70b
kfennedy <katherine.fennedy@gmail.com>
2015-12-09 20:10:08 +0800

    fix helpfulness controller

--- app/controllers/helpfulness_controller.rb
+++ app/controllers/helpfulness_controller.rb
@@ -1,12 +1,14 @@
 class HelpfulnessController < ApplicationController
 
+  before_action :prepare_product, only: [:create]
+
   def create
     begin
       query_db("INSERT INTO Helpfulness (Rating_ID, Username, Score)
           SELECT "+params[:helpfulness][:Rating_ID].to_s+" AS Rating_ID,
           '"+params[:helpfulness][:Username].to_s+"' AS Username,
           "+params[:helpfulness][:Score].to_s+" AS Score")
-      redirect_to url_for(:controller => 'products', :action=>'show', :id =>"1")
+      redirect_to url_for(:controller => 'products', :action=>'show', :id =>@product_id)
       flash[:success] = "Helpfulness successfully posted."
     rescue Exception => e
       redirect_to :back
@@ -17,5 +19,11 @@ class HelpfulnessController < ApplicationController
   def update
   end
 
+  private
+
+  def prepare_product
+    @rating_id = params[:helpfulness][:Rating_ID]
+    @product_id = query_db("SELECT Product_ID FROM Ratings WHERE rating_id = #{@rating_id}")
+  end
 
 end
