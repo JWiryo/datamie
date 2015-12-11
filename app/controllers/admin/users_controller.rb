25ab66afb9efadfd8e2261cff16bb4d82e7e3d16
kfennedy <katherine.fennedy@gmail.com>
2015-12-11 20:52:46 +0800

    admin/users/index is done

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,8 +6,4 @@ class Admin::UsersController < ApplicationController
     @users = query_db("SELECT * FROM users")
   end
 
-  def show
-    # redirect_to url_for(:controller => 'users', :action => 'show', :id => 1)
-  end
-
 end
